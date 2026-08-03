9dc343c3c00c6c0ead55d8c1d172dffa259285af
Lucas Nery Miranda <lucasnerym@gmail.com>
2026-08-03 13:43:48 -0300

    aula trabalhando com listas e coleções de dados
    Concluido:
    aula 01 Coleção de Filmes

--- 3 - trabalhando com listas e coleções de dados/ScreenMatch/src/br/com/alura/screenmatch/principal/Principal.java
+++ 3 - trabalhando com listas e coleções de dados/ScreenMatch/src/br/com/alura/screenmatch/principal/Principal.java
@@ -6,6 +6,8 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -49,5 +51,21 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        var filmeDoLucas = new Filme();
+        filmeDoLucas.setDuracaoMinutos(200);
+        filmeDoLucas.setNome("Dogville");
+        filmeDoLucas.setAnoLancamento(2003);
+        filmeDoLucas.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoLucas);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
+        System.out.println("to stringo do filme: " + listaDeFilmes.get(0).toString());
+
     }
 }
